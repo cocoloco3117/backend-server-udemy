e78e5289d87cd865c557831d432ac8777885cbd5
jfcardenasdl <jfcardenas@cads.com.gt>
2018-10-08 11:43:28 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar Variables
 var app = express();
 
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Conexion a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =>{
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
   if (err) throw err;
   console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
+/* esto se fue a routes/app.js
 app.get('/', (req, res, next) => {
   res.status(200).json({
     ok: true,
     mensaje: 'Peticion realizada correctamente'
   });
-});
+}); */
 
 // Escuchar peticiones
 app.listen(3000, () => {
